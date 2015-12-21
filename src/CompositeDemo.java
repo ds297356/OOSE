df00350414a532f121680e096fe156220f93f28f
ds297356 <ds297356@gmail.com>
2015-12-21 09:57:16 +0800

    second

--- src/CompositeDemo.java
+++ src/CompositeDemo.java
@@ -1,12 +1,59 @@
+import java.util.ArrayList; //D0392458 ªô¬f²W
+import java.util.ListIterator;
 
 public class CompositeDemo {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	}
+	
+	static void m1(Component c) {
+		for(Component[] cc: c) {
+			c.print();
+		}
+	}
+	
+}
 
+abstract class Component {
+	abstract void print();
+}
+
+class Composite extends Component {
+	
+	ArrayList<Component> list;
+	
+	public Composite() {
+		list = new ArrayList<Component>();
+	}
+	
+	void addComponent(Component c) {
+		list.add(c);
+	}
+	
+	void print() {
+		ListIterator<Component> iterator = list.listIterator();
+		
+		while(iterator.hasNext()){
+			Component c = iterator.next();
+			c.print();
+		}
+	}
+}
+
+class Question extends Component {
+	
+	String description;
+	
+	public Question(String desc) {
+		this.description = desc;
+	}
+	
+	public void print () {
+		System.out.println(description);
 	}
 
+	void addComponent(Component c) {
+		
+	}
 }
